fcdcf739fb9a424ab2aa1ba78807186013ede0dd
Igor Gutorov <igootorov@gmail.com>
2018-11-18 14:58:14 +0200

    Fix typo in comment

--- src/network.cpp
+++ src/network.cpp
@@ -9,7 +9,7 @@ Network::Network(std::size_t layers, std::size_t width) :layers{layers, Layer{wi
     // should be initialized to 0 and its width must be 28 x 28 = 784.
     this->layers.insert(this->layers.begin(), Layer{width, 784, 0});
 
-    // The last layer is always has the width of 10.
+    // The last layer always has the width of 10.
     this->layers.push_back(Layer{10, width});
 }
 
