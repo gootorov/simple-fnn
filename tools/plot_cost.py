6fe0e8e391cc615a4a06f9f7010acbed344e06fa
Igor Gutorov <igootorov@gmail.com>
2018-12-07 18:43:12 +0200

    Plot cost using moving average

--- tools/plot_cost.py
+++ tools/plot_cost.py
@@ -2,12 +2,20 @@
 import subprocess
 import seaborn as sns
 from matplotlib import pyplot as plt
+import numpy as np
+
+def moving_average(data, window):
+    data = np.cumsum(data, dtype=float)
+    data[window:] = data[window:] - data[:-window]
+    return data[window - 1:] / window
 
 def collect_data():
     compile_net = subprocess.run(['./build-release.sh'], stdout=subprocess.PIPE, shell=True, check=True)
     run_net = subprocess.run(['./run-release.sh | tail -n +5'], stdout=subprocess.PIPE, shell=True, check=True)
     output = str(run_net.stdout).split('\\n')[:-1]
     data = [float(line.split(' ')[1]) for line in output]
+    data = np.array(data)
+    data = moving_average(data, 1000)
     seaborn_data = {
         'training_example': [],
         'cost': [],
@@ -18,7 +26,8 @@ def collect_data():
     return seaborn_data
 
 def plot_data(costs):
-    plot = sns.scatterplot(x='training_example', y='cost', data=costs)
+    plot = sns.lineplot(x='training_example', y='cost', data=costs)
+    plot.set(ylabel='Cost', xlabel='Training example')
     plt.show()
 
 def main():
